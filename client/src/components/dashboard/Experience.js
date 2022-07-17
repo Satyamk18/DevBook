e8653723124a0fbea936b6ca281ddee3fd0c1f38
Satyamk18 <satyvk18@gmail.com>
2022-07-17 20:11:08 +0530

    One more commit

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -28,7 +28,7 @@ const Experience = ({ experience, deleteExperience }) => {
 
   return (
     <>
-      <h2 className='my-2'>Experience Creadentials</h2>
+      <h2 className='my-2'>Experience Credentials</h2>
 
       <table className='table'>
         <thead>
